db8465617dcb8b58a13f65853d0774f30df1d3f4
Alex Andrix <alex@andrix.fr>
2023-08-23 11:34:58 +0200

    Serve two web apps on /speaker and /assistant

--- index.js
+++ index.js
@@ -2,26 +2,28 @@
  * Alex Andrix @2023
  */
 
-import { Client } from 'node-osc';
-import express from 'express';
-import bodyParser from 'body-parser';
+import { Client } from 'node-osc'
+import express from 'express'
+import bodyParser from 'body-parser'
 
 const app = express()
-const port = 8082
-const client = new Client('127.0.0.1', 6999);
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('web'));
+const port = 8082
+const client = new Client('127.0.0.1', 6999)
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/speaker', express.static('web-speaker-controls'))
+app.use('/assistant', express.static('web-assistant-controls'))
 
 app.listen(port, () => {
-  console.log(`Confim Control app listening on port ${port} for browser input`)
+  console.log(`Confim Control speaker app listening on port ${port} for browser input`)
 })
 
 app.post('/eh', (req, res) => {
   let num = parseInt(req.body.num)
 
   // Log reception in Node console
-  console.log('HTTP POST received from browserrrrr!')
+  //console.log('HTTP POST received from browserrrrr!')
   
   // Reply to browser HTTP request with something
   res.send('OK Roger, from Node server to browser.')
@@ -32,6 +34,6 @@ app.post('/eh', (req, res) => {
   client.send(oscAddress, num, () => {
     console.log('Message with number ' + num + ' was sent on address ' + oscAddress + ' to port 6999')
     //client.close();
-  });
+  })
 
 })
---
+++ web-speaker-controls/js/browser-main.js
@@ -0,0 +1,91 @@
+/**
+ * Alex Andrix @2023
+ */
+
+// Conference tiles controls (display)
+let activate = (id) => {
+	Array.from(document.getElementsByClassName('confim-click')).forEach((el) => {
+		el.classList = "confim-click"
+
+		if (el.getAttribute('data-confim-id') == id) el.classList = "confim-click active"
+	})
+}
+// Conference tiles controls (send OSC)
+Array.from(document.querySelectorAll('[data-confim-id]')).forEach((el) => {
+	let num = el.getAttribute('data-confim-id')
+
+	el.addEventListener('click', (e) => {
+		activate(num)
+		
+		let xhr = new XMLHttpRequest()
+
+		xhr.open("POST", location.origin + "/eh") // location is the browser's protocol, hostname and port number
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+		xhr.onreadystatechange = function() {
+			if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+				
+			}
+		}
+
+		xhr.send("num=" + num)
+	})
+})
+// Special params controls (display)
+Array.from(document.getElementsByClassName('switch')).forEach((el) => {
+	el.addEventListener('click', () => {
+		el.classList = "switch " + (el.classList.contains("on") ? "off" : "on")
+	})
+})
+
+// Menu controls
+let menuNode = document.getElementById('menu')
+let titleNode = document.getElementById('title')
+let burgerNode = document.getElementById('burger')
+let bodyNode = document.getElementsByTagName('body')[0]
+let openMenu = () => {
+	burgerNode.classList = 'open'
+	titleNode.innerHTML = "Conférence Immersive - Menu"
+	menuNode.classList = ''
+	bodyNode.classList = 'frozen'
+	window.scrollTo(0, 0)
+}
+let closeMenu = () => {
+	burgerNode.classList = ''
+	titleNode.innerHTML = "Conférence Immersive"
+	menuNode.classList = 'hidden'
+	bodyNode.classList = ''
+}
+document.getElementById('burger').addEventListener('click', () => {
+	let isHidden = menuNode.classList == 'hidden'
+	if (isHidden) openMenu()
+	else closeMenu()
+})
+
+// Music play and pause toggles
+let playNode1 = document.getElementById('audio-play-1')
+let pauseNode1 = document.getElementById('audio-pause-1')
+let playNode2 = document.getElementById('audio-play-2')
+let pauseNode2 = document.getElementById('audio-pause-2')
+let playNode3 = document.getElementById('audio-play-3')
+let pauseNode3 = document.getElementById('audio-pause-3')
+let playNode4 = document.getElementById('video-play-1')
+let stopNode4 = document.getElementById('video-stop-1')
+
+let mediaNodes = [
+	[playNode1, pauseNode1],
+	[playNode2, pauseNode2],
+	[playNode3, pauseNode3],
+	[playNode4, stopNode4]
+]
+mediaNodes.forEach((couple) => {
+	let play = couple[0], pause = couple[1]
+	play.addEventListener('click', () => {
+		play.setAttribute('data-visibility', 'hidden')
+		pause.setAttribute('data-visibility', 'visible')
+	})
+	pause.addEventListener('click', () => {
+		pause.setAttribute('data-visibility', 'hidden')
+		play.setAttribute('data-visibility', 'visible')
+	})
+})
