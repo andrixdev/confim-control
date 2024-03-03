53c8c88b6f1bbcb86774c9ea405d6e85c30e061a
Alex Andrix <alex@andrix.fr>
2024-03-03 16:07:48 +0100

    Add several new chapters' images and controls

--- web-speaker-controls/js/speaker-browser-main.js
+++ web-speaker-controls/js/speaker-browser-main.js
@@ -88,6 +88,21 @@ Array.from(document.querySelectorAll('[data-confim-id]')).forEach((el) => {
 })
 
 // OSC send on slider input changes
+// Screen position
+document.getElementById('screen-position-range').addEventListener('input', (ev) => {
+	let num = ev.target.value // {0, 1, 2}
+
+	let xhr = new XMLHttpRequest()
+	xhr.open("POST", location.origin + "/eh") // location is the browser's protocol, hostname and port number
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+	// 114 -> OFF
+	// 115 -> FAR
+	// 116 -> CLOSE
+	let oscRangeOffset = 114 
+	num = oscRangeOffset - (-num)
+	xhr.send("num=" + num)
+})
 // Brain opacity
 let brainOpaNode = document.getElementById('opa')
 document.getElementById('brain-opacity-range').addEventListener('input', (ev) => {
