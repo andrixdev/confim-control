aba1fa72eb585d58faa5b284bacfe76688a61dfe
Alex Andrix <alex@andrix.fr>
2024-04-23 11:42:48 +0200

    Add a loadbar and an overlay to prevent double-taps

--- sources/js/browser-main.js
+++ sources/js/browser-main.js
@@ -7,6 +7,7 @@ Array.from(document.querySelectorAll('[data-confim-id]')).forEach((el) => {
 	let num = el.getAttribute('data-confim-id')
 
 	el.addEventListener('click', (e) => {
+		// Send XHR with OSC number
 		let xhr = new XMLHttpRequest()
 		xhr.open("POST", location.origin + "/eh") // location is the browser's protocol, hostname and port number
 		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -14,6 +15,10 @@ Array.from(document.querySelectorAll('[data-confim-id]')).forEach((el) => {
 			if (this.readyState === XMLHttpRequest.DONE && this.status === 200) { }
 		}
 		xhr.send("num=" + num)
+
+		// Front-end loading time
+		if (num >= 1 && num <= 49) animateLoadbar(2000) // 2 seconds (see Unity Chameleon.cs duration)
+		else animateLoadbar(250) // Enough to prevent double-click
 	})
 })
 
@@ -39,3 +44,18 @@ Array.from(document.getElementsByClassName('switch')).forEach((el) => {
 		})
 	})
 })
+
+// Loadbar animation
+let load = document.getElementById('loadbar-inner')
+let overlay = document.getElementById('overlay')
+let animateLoadbar = (duration) => {
+	overlay.style.display = 'inherit';
+	load.style.transitionDuration = duration + 'ms'
+	load.style.width = '100%'
+
+	setTimeout(() => {
+		load.style.transitionDuration = '1ms'
+		load.style.width = '0%'
+		overlay.style.display = 'none'
+	}, duration)
+}
