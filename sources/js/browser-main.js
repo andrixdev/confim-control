09f7013ea706389389f8c01d188c72d393b26b3c
Alex Andrix <alex@andrix.fr>
2024-09-06 11:56:20 +0200

    Add loadbar with different loading times and add overlay to prevent multi-taps

--- sources/js/browser-main.js
+++ sources/js/browser-main.js
@@ -15,6 +15,10 @@ Array.from(document.querySelectorAll('[data-confim-id]')).forEach((el) => {
 			if (this.readyState === XMLHttpRequest.DONE && this.status === 200) { }
 		}
 		xhr.send("num=" + num)
+
+		// Front-end loading time
+		if (num >= 1 && num <= 49) animateLoadbar(2000) // 2 seconds (see Unity Chameleon.cs duration)
+		else animateLoadbar(250) // Enough to prevent double-click
 	})
 })
 
@@ -24,3 +28,18 @@ Array.from(document.getElementsByClassName('switch')).forEach((el) => {
 		el.classList = "switch " + (el.classList.contains("on") ? "off" : "on")
 	})
 })
+
+// Loadbar animation
+let load = document.getElementById('loadbar-inner')
+let overlay = document.getElementById('overlay')
+let animateLoadbar = (duration) => {
+	overlay.style.display = 'inherit';
+	load.style.transitionDuration = duration + 'ms'
+	load.style.width = '100%'
+
+	setTimeout(() => {
+		load.style.transitionDuration = '1ms'
+		load.style.width = '0%'
+		overlay.style.display = 'none'
+	}, duration)
+}
