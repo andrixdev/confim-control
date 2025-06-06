7ea0027658f33303949256543f2dfb15409f1ae6
Alex Andrix <alex@andrix.fr>
2025-06-06 12:14:28 +0200

    Add new foam img controls and several audio tracks to test

--- web-assistant-controls/js/browser-assistant.js
+++ web-assistant-controls/js/browser-assistant.js
@@ -51,14 +51,23 @@ let playNode2 = document.getElementById('audio-play-2')
 let stopNode2 = document.getElementById('audio-stop-2')
 let playNode3 = document.getElementById('audio-play-3')
 let stopNode3 = document.getElementById('audio-stop-3')
-let playNode5 = document.getElementById('video-play-1')
-let stopNode5 = document.getElementById('video-stop-1')
+let playNode4 = document.getElementById('audio-play-4')
+let stopNode4 = document.getElementById('audio-stop-4')
+let playNode5 = document.getElementById('audio-play-5')
+let stopNode5 = document.getElementById('audio-stop-5')
+let playNode6 = document.getElementById('audio-play-6')
+let stopNode6 = document.getElementById('audio-stop-6')
+let playNode7 = document.getElementById('video-play-1')
+let stopNode7 = document.getElementById('video-stop-1')
 
 let mediaNodes = [
 	[playNode1, stopNode1],
 	[playNode2, stopNode2],
 	[playNode3, stopNode3],
-	[playNode5, stopNode5]
+	[playNode4, stopNode4],
+	[playNode5, stopNode5],
+	[playNode6, stopNode6],
+	[playNode7, stopNode7]
 ]
 mediaNodes.forEach((couple) => {
 	let play = couple[0], stop = couple[1]
